9979099f5aeae5efd2a26e7e5993093516a9eeff
Arka Sahoo <arkasahoo2005@gmail.com>
2025-12-07 22:38:35 -0600

    Added code

--- logic.py
+++ logic.py
@@ -9,8 +9,36 @@ class Logic(QMainWindow, Ui_MainWindow):
         self.pushButton.clicked.connect(lambda : self.submit())
 
     def submit(self):
+        jane_total = 0
+        john_total = 0
         csvfile="votes.csv"
         with open(csvfile, 'a', newline='') as file:
             file.write('Voter  Candidate  Total\n')
-            name = self.input_id.text()
-
+            name = self.input_id.text().strip()
+            is_number = False
+            try:
+                number = int(name)
+                is_number = True
+            except ValueError:
+                self.label_message.setText('Invalid ID. Please enter your correct ID.')
+            if is_number:
+                try:
+                    if (number >= 10000) and (number <= 99999):
+                        self.label_message.setText('ID is valid. Please vote any one candidate.')
+                    else:
+                        self.label_message.setStyleSheet("color: red;")
+                        self.label_message.setText('Invalid ID. Please enter your correct ID.')
+                except NameError:
+                    self.label_message.setStyleSheet("color: red;")
+                    self.label_message.setText('Invalid ID. Please enter your correct ID.')
+            else:
+                self.label_message.setStyleSheet("color: red;")
+                self.label_message.setText('Invalid ID. Please enter your correct ID.')
+            if self.radioButton_jane.isChecked():
+                jane_total += 1
+                self.label_message.setText(f'Voted Jane, Total - {jane_total}')
+            elif self.radioButton_john.isChecked():
+                john_total += 1
+                self.label_message.setText(f'Voted John, Total - {john_total}')
+            else:
+                self.label_message.setText('Please vote any one candidate.')
