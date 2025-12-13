ab18c661103bb647eb96e2bb4d111c50a62c3f38
Arka Sahoo <arkasahoo2005@gmail.com>
2025-12-12 20:23:39 -0600

    Added docstrings and comments where needed

--- logic.py
+++ logic.py
@@ -20,12 +20,12 @@ class Logic(QMainWindow, Ui_MainWindow):
 
         self.pushButton.clicked.connect(lambda : self.submit())
 
-        self.jane_total = 0
-        self.john_total = 0
+        self.jane_total = 0 # Variable for calculating Jane's total number of votes
+        self.john_total = 0 # Variable for calculating John's total number of votes
 
         my_file = 'votes.csv'
         with open(my_file, 'w') as file:
-            file.write('Voter           Candidate           Total\n')
+            file.write('Voter           Candidate           Total\n') # Writing the header.
 
 
     def submit(self):
@@ -33,18 +33,18 @@ class Logic(QMainWindow, Ui_MainWindow):
         This method retrieves the user input, performs validation checks, and then processes the data.
         """
         csvfile = "votes.csv"
-        already_voted = False
-        name = self.input_id.text().strip()
-        is_number = False
+        already_voted = False # A boolean variable whose initial value is False
+        name = self.input_id.text().strip() # Taking the ID from the user as input
+        is_number = False # A boolean variable whose initial value is False. If the ID entered by the user is a number, it becomes True
         with open(csvfile, 'a', newline='') as file:
             try:
                 number = int(name)
                 is_number = True
             except ValueError:
                 self.label_message.setText('Invalid ID. Please enter your correct ID.')
-            if is_number:
+            if is_number: # If the ID is a number
                 try:
-                    if (number >= 10000) and (number <= 99999):
+                    if (number >= 10000) and (number <= 99999): # Checking if the ID is a five-digit number
                         self.label_message.setStyleSheet("color: black;")
                         self.label_message.setText('ID is valid. Please vote any one candidate.')
                         with open('votes.csv', 'r') as f:
@@ -52,22 +52,22 @@ class Logic(QMainWindow, Ui_MainWindow):
                             for row in csv_reader:
                                 if row and str(number) in row[0]:
                                     already_voted = True
-                                    self.label_message.setStyleSheet("color: red;")
+                                    self.label_message.setStyleSheet("color: red;") # Setting the text color to red for error messages
                                     self.label_message.setText(f'Already voted')
                         if self.radioButton_jane.isChecked() and already_voted == False:
-                            self.jane_total += 1
+                            self.jane_total += 1 # Jane's total increases by 1
                             self.label_message.setText(f'Voted Jane')
                             file.write(f'Voter - {number}  Candidate - Jane  Total - {self.jane_total}\n')
                         elif self.radioButton_john.isChecked() and already_voted == False:
-                            self.john_total += 1
+                            self.john_total += 1 # John's total increases by 1
                             self.label_message.setText(f'Voted John')
                             file.write(f'Voter - {number}  Candidate - John  Total - {self.john_total}\n')
                     else:
-                        self.label_message.setStyleSheet("color: red;")
+                        self.label_message.setStyleSheet("color: red;") # Setting the text color to red for error messages
                         self.label_message.setText('Invalid ID. Please enter your correct ID.')
                 except NameError:
-                    self.label_message.setStyleSheet("color: red;")
+                    self.label_message.setStyleSheet("color: red;") # Setting the text color to red for error messages
                     self.label_message.setText('Invalid ID. Please enter your correct ID.')
             else:
-                self.label_message.setStyleSheet("color: red;")
+                self.label_message.setStyleSheet("color: red;") # Setting the text color to red for error messages
                 self.label_message.setText('Invalid ID. Please enter your correct ID.')
