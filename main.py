14f267323397ab0bbe1aa41662ef242e8a6f1be7
Arka Sahoo <arkasahoo2005@gmail.com>
2025-12-20 11:17:53 -0600

    Added parameters and returns to docstrings.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from logic import *
 def main() -> None:
     """
     The main function for the PyQt6 application.
+    :param: No parameters.
+    :return: None.
     """
     application = QApplication([])
     window = Logic()
